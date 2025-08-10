702da3a35c34fdf8de4c0450fcef3d723292920f
CarlosLooopez <carlosalopez357@gmail.com>
2025-08-10 22:51:46 +0000

    cartas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
-import "bootstrap";
-import "./style.css";
+ let valores = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+        let palos = ['♠', '♥', '♦', '♣'];
 
+        // Generar carta aleatoria
+        function generarCarta() {
+            let valorAleatorio = valores[Math.floor(Math.random() * valores.length)];
+            let paloAleatorio = palos[Math.floor(Math.random() * palos.length)];
+            
+            // Determinar color
+            let color = (paloAleatorio === '♥' || paloAleatorio === '♦') ? 'red' : 'black';
+            
+            // Mostrar en las tres posiciones
+            document.getElementById('topLeft').innerHTML = valorAleatorio + '<br>' + paloAleatorio;
+            document.getElementById('center').textContent = paloAleatorio;
+            document.getElementById('bottomRight').innerHTML = valorAleatorio + '<br>' + paloAleatorio;
+            
+            // Aplicar color
+            document.getElementById('topLeft').style.color = color;
+            document.getElementById('center').style.color = color;
+            document.getElementById('bottomRight').style.color = color;
+        }
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+        // Generar carta al cargar la página
+        generarCarta();
